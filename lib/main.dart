2b300b0bb0547091f45fe7de7abefd3443c64aa9
Rizwan Islam khan <86212958+rizwan-khan786@users.noreply.github.com>
2024-01-15 14:26:16 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:movie_ticket_booking/ui/screens/home/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:HomePage(),
+    );
+  }
+}
